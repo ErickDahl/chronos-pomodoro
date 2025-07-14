390750fa54312e4791eeb552ae25a68d3e79e2ee
Erick <erickdahl1@gmail.com>
2025-07-14 15:31:39 -0300

    feat: implement CountDown component with styling and integrate into App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
+import { CountDown } from './components/CountDown';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
@@ -14,6 +15,10 @@ function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+const CountDown = () => {
+  return (
+    <div className={styles.countDown}>
+      <span>00:00</span>
+    </div>
+  );
+};
+
+export { CountDown };
